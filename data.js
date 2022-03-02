da64b755faf45b4e2680b068c74d4e93cb46c229
rayjosong <rayjosong@gmail.com>
2022-03-02 23:11:40 +0800

    added routers for Account – get, post, del. Missing update for now

--- data.js
+++ data.js
@@ -43,8 +43,103 @@ function add_user(user) {
   users.push(user);
 }
 
+// -------- ACCOUNTS --------
+
+let accounts = [
+  {
+    type: "saving",
+    balance: 100,
+    max_limit: 500,
+    create_date: "2022-02-26",
+    holder: "qwe",
+    account_no: "1234567890",
+  },
+  {
+    type: "saving",
+    balance: 100000,
+    max_limit: 1000000,
+    create_date: "2022-02-27",
+    holder: "qsdf sdfg",
+    account_no: "1234567891",
+  },
+  {
+    type: "business",
+    balance: 1000093784,
+    max_limit: 100000000000,
+    create_date: "2022-02-28",
+    holder: "hdg jvh",
+    account_no: "1234567892",
+  },
+];
+
+function get_all_accounts() {
+  return accounts;
+}
+
+// when input account holder, return all accounts that this holder has (each AccHolder can have > 1 accounts )
+function get_accs_by_holder(accHolder) {
+  let accountArray = [];
+
+  for (i = 0; i < accounts.length; i++) {
+    if (accounts[i].holder == accHolder) {
+      accountArray.push(accounts[i]);
+    }
+  }
+  return accountArray;
+}
+function get_acc_by_accNo(accountNo) {
+  for (i = 0; i < accounts.length; i++) {
+    if (accounts[i].account_no.toString() == accountNo.toString()) {
+      let account = accounts[i];
+      return account;
+    }
+  }
+}
+
+function del_acc_by_accNo(accountNo) {
+  for (i = 0; i < accounts.length; i++) {
+    if (accounts[i].account_no == accountNo) {
+      accounts.splice(i, 1); // splice(index, howMany) see w3schools documentation
+    }
+  }
+}
+// console.log(get_acc_by_accNum("1234567892"));
+
+// add new account
+function add_acc(account) {
+  accounts.push(account);
+}
+
+// -------- TRANSACTIONS --------
+
+let transactions = [
+  {
+    date: "2022-01-28",
+    type: "account management",
+    account_no: "1234567890",
+    amount: 200,
+  },
+  {
+    date: "2022-01-28",
+    type: "investment",
+    account_no: "1234567891",
+    amount: 5000,
+  },
+  {
+    date: "2022-01-28",
+    type: "investment",
+    account_no: "1234567892",
+    amount: 150000,
+  },
+];
+
 module.exports = {
   add_user,
   get_all_users,
   get_user_by_user_id,
+  get_all_accounts,
+  get_accs_by_holder,
+  add_acc,
+  // get_acc_by_accNum,
+  del_acc_by_accNo,
 };
--- routers.js
+++ routers.js
@@ -8,16 +8,18 @@ const database = require("./data");
 // Define router object
 let router = express.Router();
 
+// ------------------- USERS -------------------
+
 //Define an API to return all the users
 router.get("/users/all", (request, response) => {
-    let users = database.get_all_users();
-    response.send(users);
+  let users = database.get_all_users();
+  response.send(users);
 });
 
 // Define an API to get user based on user_id passed in the request
 router.get("/users/by-uid", (request, response) => {
-    let users = database.get_user_by_user_id(request.query.uid);
-    response.send(users);
+  let users = database.get_user_by_user_id(request.query.uid);
+  response.send(users);
 });
 
 // Define a POST API to add a new user to database
@@ -29,19 +31,53 @@ router.post("/users/add", (request, response) => {
 // Define a GET API with path "/home"
 // router.get("path", callback);
 router.get("", (request, response) => {
-    response.send("Welcome!");
+  response.send("Welcome!");
 });
 
 // Define a GET API with path "/sum"
 router.get("/sum", (request, response) => {
-    let sum = parseInt(request.query.a) + parseInt(request.query.b);
-    response.send("Sum is: " + sum);
+  let sum = parseInt(request.query.a) + parseInt(request.query.b);
+  response.send("Sum is: " + sum);
 });
 
 // Define a POST API with path "/sum"
 router.post("/sum", (request, response) => {
-    let sum = request.body.a + request.body.b;
-    response.send("Sum is : " + sum);
+  let sum = request.body.a + request.body.b;
+  response.send("Sum is : " + sum);
 });
+
+// ------------------- ACCOUNTS -------------------
+
+// Define an API to return all accounts
+router.get("/accounts/all", (request, response) => {
+  let accounts = database.get_all_accounts();
+  response.send(accounts);
+});
+
+// Define an API to return accounts when input account holder
+router.get("/accounts/by-accholder", (request, response) => {
+  let accounts = database.get_accs_by_holder(request.query.accholder);
+  response.send(accounts);
+});
+
+router.get("/accounts/by-acc-no", (request, response) => {
+  let accounts = database.get_accs_by_holder(request.query.acc_no);
+  response.send(accounts);
+}); // still not working
+
+// Define a POST API to add a new account to database
+router.post("/accounts/add", (request, response) => {
+  let accounts = database.add_acc(request.body.acc_no);
+  response.send("account added.");
+});
+
+// Define a DEL API
+
+router.delete("/accounts/delete", (request, response) => {
+  let account_to_del = database.del_acc_by_accNo(request.query.acc_no);
+  response.send(`Account ${account_to_del} deleted`);
+});
+
+// ------------------- TRANSACTIONS -------------------
 
 module.exports = { router };
