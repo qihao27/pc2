c9ffd4f8b035a3cc4682012a7a4b06f3b20d7313
Qihao <wuqihao07@gmail.com>
2022-03-09 06:11:07 +0800

    update frontend/middleware minor bug fixing

--- public/getdata.js
+++ public/getdata.js
@@ -1,30 +1,13 @@
-const deposiitbalance = document.getElementById("deposit");
-const investmentbalance = document.getElementById("investment");
-
+// const deposiitbalance = document.getElementById("deposit");
+// const investmentbalance = document.getElementById("investment");
 const accountBalance = document.getElementById("account_balance");
+// const url = "https://fintech-group4.herokuapp.com";  // live site
+const url = "http://localhost:3000";  // for testing purpose only
 
 let init_deposit_balance = 0.0;
 let init_investment_balance = 5000.0; //hardcoded for now
-
 let user_id = 922;
 
-//const b1 = document.getElementById("b1");
-// const url = "https://fintech-group4.herokuapp.com";
-const url = "http://localhost:3000";
-//b1.addEventListener("click", () => {
-//let user_id = document.getElementById("inputbox").value;
-//$.getJSON(`${url}/users/by-uid?uid=${user_id}`, (data) => {
-//console.log(data);
-// data passed in as an object of a list
-//data = data[0];
-//let code = `First Name: ${data.first_name} <br>
-//          Last Name:  ${data.last_name} <br>
-//         Email:      ${data.email}`;
-//$(".mypanel").html(code);
-//});
-//});
-
-// b1 = deposit | b2 = buy | b3 = sell | b4 = transaction hist
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
 
@@ -44,21 +27,64 @@ function drawChart() {
   chart.draw(data, options);
 }
 
-// transaction history
-const b4 = document.getElementById("b4");
-b4.addEventListener("click", () => {
-  $.getJSON(
-    `${url}/transactions/by-account-id?account_id=${user_id}`,
-    (data) => {
-      console.log(data);
+// deposite button control
+const deposit_btn = document.getElementById("deposit_btn");
+deposit_btn.addEventListener("click", () => {
+  // let user_id = 922;
+  let amount = document.getElementById("deposit_amount").value;
+  $.getJSON(`${url}/transactions/deposit?amount=${amount}`, () => {
+    alert(`You have successfully deposited $${amount}.`);
+  });
 
+  init();
+});
+
+// buy button control
+const buy_btn = document.getElementById("buy_btn");
+buy_btn.addEventListener("click", () => {
+  console.log("buying");
+  // let user_id = 922;
+  let amount = document.getElementById("investment_amount").value;
+  $.getJSON(`${url}/transactions/balance`, (data) => { 
+    let balance = data[0].balance;
+    if (balance >= amount) {
+      $.getJSON(`${url}/transactions/buy?amount=${amount}`, () => {
+        alert(`You have made an investment of $${amount}`);
+      });
+    } else {
+      alert(`Insufficient found!`);
+    }
+  });
+
+  init();
+});
+
+// sell button control
+const sell_btn = document.getElementById("sell_btn");
+sell_btn.addEventListener("click", () => {
+  console.log("selling");
+  // let user_id = 922;
+  let amount = document.getElementById("investment_amount").value;
+  $.getJSON(`${url}/transactions/sell?amount=${amount}`, () => {
+    alert(`You have sold $${amount} worth of assets`);
+  });
+
+  init();
+});
+
+
+// transaction history
+const history_btn = document.getElementById("history_btn");
+history_btn.addEventListener("click", () => {
+  $.getJSON(
+    `${url}/transactions/by-account-id?account_id=${user_id}`, (data) => {
+      console.log(data);
       let code = "<ul>";
-      // 6 March: need to change format for transactions history
-      //
+      // TODO: 6 March - change format for transactions history
       data.forEach((datapoint) => {
         let date = datapoint.transaction_date.substring(0, 10);
         code += `<li> Date: ${date} 
-                    | Amount: $${datapoint.amount}
+                    | Amount Transacted: $${datapoint.amount}
                     </li>`;
       });
       code += "</ul>";
@@ -68,88 +94,6 @@ b4.addEventListener("click", () => {
 
   init();
 });
-
-// b1 = deposit
-const b1 = document.getElementById("b1");
-b1.addEventListener("click", () => {
-  let user_id = 922;
-  let amount = document.getElementById("deposit_amount").value;
-  $.getJSON(`${url}/transactions/by-deposit-amt?amount=${amount}`, () => {
-    // console.log(data);
-    // let code = "<ul>";
-    // code += "Funds deposited.";
-    // code += "</ul>";
-    // $(".mypanel").html(code);
-    alert(`You have successfully deposited $${amount}.`);
-  });
-
-  init();
-});
-
-//b2 = buying
-const b2 = document.getElementById("b2");
-b2.addEventListener("click", () => {
-  console.log("clicked");
-  let user_id = 922;
-  let amount = document.getElementById("investment_amount").value;
-  $.getJSON(`${url}/transactions/buy?amount=${amount}`, () => {
-    // console.log(data);
-
-    let code = "<ul>";
-    code += "Investment made.";
-    code += "</ul>";
-
-    $(".mypanel").html(code);
-  });
-  alert(`You have made an investment of ${amount}`);
-  init();
-});
-
-//b3 = selling
-const b3 = document.getElementById("b3");
-b3.addEventListener("click", () => {
-  console.log("selling");
-  let user_id = 922;
-  let amount = document.getElementById("investment_amount").value;
-  $.getJSON(`${url}/transactions/sell?amount=${amount}`, () => {
-    // console.log(data);
-
-    let code = "<ul>";
-    code += "Sold.";
-    code += "</ul>";
-
-    $(".mypanel").html(code);
-  });
-  alert(`You have sold ${amount} worth of assets`);
-  init();
-});
-
-// const b1 = document.getElementById("b1"); // deposit
-// b1.addEventListener("click", () => {
-//   //console.log("test");
-//   let new_deposit = document.getElementById("deposit_amount").value; //HC
-//   // //$.getJSON(`${url}/transactions/by-uid?uid=${user_id}`, (data) => {
-//   // // console.log(data);
-//   // // let code = "<ul>";
-//   // // 6 March: need to change format for transactions history
-//   // //
-//   // //data.forEach((datapoint) => {
-//   // //  code += `<li> First Name: ${datapoint.first_name}
-//   // //                | Last Name: ${datapoint.last_name}
-//   // //               | Email: ${datapoint.email} </li>`;
-//   // //});
-//   // //code += "</ul>";
-//   // // $(".mypanel").html(code);
-//   // // //});
-//   // // let testingcode = "<ul><li>";
-//   // // testingcode += new_deposit;
-//   // // testingcode += "</li></ul>";
-//   // // $(".mypanel").html(testingcode);
-//   // // init_deposit_balance += new_deposit;
-//   // console.log(new_deposit);
-
-//   init();
-// });
 
 function init() {
   let temp_amount = 0.0;
--- routers.js
+++ routers.js
@@ -6,8 +6,8 @@ require("dotenv").config();
 const { connection } = require("./database");
 
 const { auth, requiresAuth } = require("express-openid-connect");
-const { connect } = require("http2");
 const query = require("express/lib/middleware/query");
+
 router.use(
   auth({
     authRequired: false,
@@ -22,115 +22,26 @@ router.use(
 
 router.use("/public", express.static(path.join(__dirname, "public")));
 
-// ------------------- Auth0 -------------------
+// ------------------- Default -------------------
 router.get("/", (request, response) => {
   if (request.oidc.isAuthenticated()) {
     response.redirect("/index");
   } else {
-    response.send("Logged out");
+    response.redirect("/login");
   }
-});
-
-// GET API with path "/home"
-router.get("/home", requiresAuth(), (request, response) => {
-  response.send("Welcome!");
 });
 
 router.get("/index", (request, response) => {
   response.sendFile(path.join(__dirname + "/index.html"));
 });
 
-// ------------------- USERS -------------------
-// GET all users
-router.get("/users/all", requiresAuth(), (request, response) => {
-  connection.query("select * from users", (error, result) => {
-    if (error) {
-      console.log(error);
-      response.status(500).send("Something went wrong...");
-    } else {
-      response.status(200).send(result);
-    }
-  });
-});
-
-// GET user based on uid passed in the request
-router.get("/users/by-uid", requiresAuth(), (request, response) => {
-  connection.query(
-    `select * from users where uid=${request.query.uid}`,
-    (error, result) => {
-      if (error) {
-        console.log(error);
-        response.status(500).send("Something went wrong...");
-      } else {
-        result.length == 0
-          ? response.status(404).send("user not found")
-          : response.status(200).send(result);
-      }
-    }
-  );
-});
-
-// Define a POST API to add a new user to database
-router.post("/users/add", requiresAuth(), (request, response) => {
-  connection.query(
-    `insert into users (first_name, last_name, email) 
-    values ("${request.body.first_name}", "${request.body.last_name}", "${request.body.email}")`,
-    (error, result) => {
-      if (error) {
-        console.log(error);
-        response.status(500).send("Something went wrong...");
-      } else {
-        response.status(200).send("User added to the database!");
-      }
-    }
-  );
-});
-
-// ------------------- ACCOUNTS -------------------
-
-// // Define an API to return all accounts
-// router.get("/accounts/all", requiresAuth(), (request, response) => {
-//   let accounts = database.get_all_accounts();
-//   response.send(accounts);
-// });
-
-// // Define an API to return accounts when input account holder
-// router.get("/accounts/by-accholder", requiresAuth(), (request, response) => {
-//   let accounts = database.get_accs_by_holder(request.query.accholder);
-//   response.send(accounts);
-// });
-
-// router.get("/accounts/by-acc-no", requiresAuth(), (request, response) => {
-//   let accounts = database.get_accs_by_holder(request.query.acc_no);
-//   response.send(accounts);
-// }); // still not working
-
-// Define a POST API to add a new account to database
-// router.post("/accounts/add", requiresAuth(), (request, response) => {
-//   let accounts = database.add_acc(request.body.acc_no);
-//   response.send("account added.");
-// });
-
-// Define a DEL API
-
-// router.delete("/accounts/delete", requiresAuth(), (request, response) => {
-//   let account_to_del = database.del_acc_by_accNo(request.query.acc_no);
-//   response.send(`Account ${account_to_del} deleted`);
-// });
-
 // ------------------- TRANSACTIONS -------------------
-
-// router.get("/transactions/all", requiresAuth(), (request, response) => {
-//   let transactions = database.get_all_transactions();
-//   response.send(transactions);
-// });
-
 router.get(
   "/transactions/by-account-id",
   requiresAuth(),
   (request, response) => {
     connection.query(
-      `select type, amount, date(transaction_date) as transaction_date, account_id from transactions where account_id=${request.query.account_id} `,
+      `select * from transactions where account_id=${request.query.account_id} `,
       (error, result) => {
         if (error) {
           console.log(error);
@@ -147,7 +58,7 @@ router.get(
 
 // POST API for deposits
 router.get(
-  "/transactions/by-deposit-amt",
+  "/transactions/deposit",
   requiresAuth(),
   (request, response) => {
     var currentdate = new Date();
@@ -244,5 +155,83 @@ router.get("/transactions/balance", requiresAuth(), (request, response) => {
     }
   );
 });
+
+// ------------------- USERS -------------------
+// GET all users
+/*router.get("/users/all", requiresAuth(), (request, response) => {
+  connection.query("select * from users", (error, result) => {
+    if (error) {
+      console.log(error);
+      response.status(500).send("Something went wrong...");
+    } else {
+      response.status(200).send(result);
+    }
+  });
+});
+
+// GET user based on uid passed in the request
+router.get("/users/by-uid", requiresAuth(), (request, response) => {
+  connection.query(
+    `select * from users where uid=${request.query.uid}`,
+    (error, result) => {
+      if (error) {
+        console.log(error);
+        response.status(500).send("Something went wrong...");
+      } else {
+        result.length == 0
+          ? response.status(404).send("user not found")
+          : response.status(200).send(result);
+      }
+    }
+  );
+});
+
+// Define a POST API to add a new user to database
+router.post("/users/add", requiresAuth(), (request, response) => {
+  connection.query(
+    `insert into users (first_name, last_name, email) 
+    values ("${request.body.first_name}", "${request.body.last_name}", "${request.body.email}")`,
+    (error, result) => {
+      if (error) {
+        console.log(error);
+        response.status(500).send("Something went wrong...");
+      } else {
+        response.status(200).send("User added to the database!");
+      }
+    }
+  );
+});
+
+// ------------------- ACCOUNTS -------------------
+
+// Define an API to return all accounts
+router.get("/accounts/all", requiresAuth(), (request, response) => {
+  let accounts = database.get_all_accounts();
+  response.send(accounts);
+});
+
+// Define an API to return accounts when input account holder
+router.get("/accounts/by-accholder", requiresAuth(), (request, response) => {
+  let accounts = database.get_accs_by_holder(request.query.accholder);
+  response.send(accounts);
+});
+
+router.get("/accounts/by-acc-no", requiresAuth(), (request, response) => {
+  let accounts = database.get_accs_by_holder(request.query.acc_no);
+  response.send(accounts);
+}); // still not working
+
+Define a POST API to add a new account to database
+router.post("/accounts/add", requiresAuth(), (request, response) => {
+  let accounts = database.add_acc(request.body.acc_no);
+  response.send("account added.");
+});
+
+Define a DEL API
+
+router.delete("/accounts/delete", requiresAuth(), (request, response) => {
+  let account_to_del = database.del_acc_by_accNo(request.query.acc_no);
+  response.send(`Account ${account_to_del} deleted`);
+}); */
 
 module.exports = { router };
