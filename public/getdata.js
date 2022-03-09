cf1a48371728c30a3b548a904784267ca9c859d6
Qihao <wuqihao07@gmail.com>
2022-03-10 01:57:14 +0800

    update API auto update investment in db update buy/sell btn

--- public/getdata.js
+++ public/getdata.js
@@ -1,27 +1,38 @@
-// const deposiitbalance = document.getElementById("deposit");
 const account_balance = document.getElementById("account_balance");
 const investment = document.getElementById("investment");
 const deposit_btn = document.getElementById("deposit_btn");
-const history_btn = document.getElementById("history_btn");
-// const url = "https://fintech-group4.herokuapp.com";  // live site
-const url = "http://localhost:3000"; // for testing purpose only
-
-let init_investment_balance = 5000.0; //hardcoded for now
+const buy_btn = document.getElementById("buy_btn");
+const sell_btn = document.getElementById("sell_btn");
+const url = "https://fintech-group4.herokuapp.com";  // live site
+// const url = "http://localhost:3000"; // for testing purpose only
 
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart);
 
 function init() {
   $.getJSON(`${url}/transactions/history`, (data) => {
-    console.log(data);
+    console.log(data[0]);
+    console.log(data[1]);
     let code = "<ul>";
-    let balance = data[0].balance;
+    let balance = data[0][0].balance;
     account_balance.innerText = "$" + balance.toFixed(2);
 
-    if (data[0].amount != null) {
-      data.forEach((datapoint) => {
+    let invest_amount = 0.00;
+    if (data[1].length != 0) {
+      if (typeof data[1][1] != 'undefined') {
+        invest_amount = data[1][0].invest - data[1][1].invest;
+      } else {
+        invest_amount = data[1][0].invest;
+      }
+    }
+    investment.innerText = "$" + invest_amount.toFixed(2);
+
+    if (data[0][0].amount != null) {
+      data[0].forEach((datapoint) => {
         let date = datapoint.transaction_date.substring(0, 10);
-        code += `<li>Date: ${date} | Amount Transacted: $${datapoint.amount}</li>`;
+        code += `<li>Date: ${date} | Amount Transacted: `;
+        code += (datapoint.type == 10) ? `-` : ``;
+        code += `$${datapoint.amount}</li>`;
       });
     } else {
       code += `<li> No transaction made yet...</li>`;
@@ -29,72 +40,48 @@ function init() {
     code += "</ul>";
     $(".mypanel").html(code);
   });
-
-  // $("#btn").submit(function (e) {
-  //   e.preventDefault();
-  // });
 
   drawChart();
-  //investment.innerText = `$${init_investment_balance}`;
 }
-
-// transaction history button control
-/*history_btn.addEventListener("click", () => {
-  $.getJSON(`${url}/transactions/history`, (data) => {
-    console.log(data);
-    let code = "<ul>";
-    if (data[0].amount != null) {
-      data.forEach((datapoint) => {
-        let date = datapoint.transaction_date.substring(0, 10);
-        code += `<li>Date: ${date} | Amount Transacted: $${datapoint.amount}</li>`;
-      });
-    } else {
-      code += `<li> No transaction made yet...</li>`;
-    }
-    code += "</ul>";
-    $(".mypanel").html(code);
-  });
-
-  init();
-});*/
 
 // top up button control
 deposit_btn.addEventListener("click", () => {
   let amount = document.getElementById("deposit_amount").value;
   $.getJSON(`${url}/transactions/deposit?amount=${amount}`);
-  alert(`You have successfully deposited $${amount}.`);
-  console.log("deposit successful");
+  !amount ? alert(`Please input amount to be deposited into account.`) : 
+    alert(`You have successfully deposited $${amount}.`);
+
   init();
 });
 
 // buy button control
-const buy_btn = document.getElementById("buy_btn");
 buy_btn.addEventListener("click", () => {
-  console.log("buying");
-  // let user_id = 922;
   let amount = document.getElementById("investment_amount").value;
-  let balance = 0;
-  $.getJSON(`${url}/transactions/balance`, (data) => {
-    balance = data[0].balance;
-    if (balance >= amount) {
+  // $.getJSON(`${url}/balance`, (data) => {
+  //   let balance = data[0].balance;
+  //   if (balance >= amount) {
       $.getJSON(`${url}/transactions/buy?amount=${amount}`);
       alert(`You have made an investment of $${amount}`);
-    } else {
-      alert(`Insufficient funds!`);
-    }
-  });
+    // } else {
+    //   alert(`Insufficient funds!`);
+    // }
+  // });
 
   init();
 });
 
 // sell button control
-const sell_btn = document.getElementById("sell_btn");
 sell_btn.addEventListener("click", () => {
-  console.log("selling");
-  // let user_id = 922;
   let amount = document.getElementById("investment_amount").value;
-  $.getJSON(`${url}/transactions/sell?amount=${amount}`);
-  alert(`You have sold $${amount} worth of assets`);
+  // $.getJSON(`${url}/investment`, (data) => {
+  //   let invest = data[0].invest;
+  //   if (invest >= amount) {
+      $.getJSON(`${url}/transactions/sell?amount=${amount}`);
+      alert(`You have sold $${amount} worth of assets`);
+    // } else {
+    //   alert(`Insufficient funds!`);
+    // }
+  // });
 
   init();
 });
@@ -104,7 +91,7 @@ function drawChart() {
   var data = google.visualization.arrayToDataTable([
     ["Item", "$"],
     ["Deposit", parseInt(account_balance)],
-    ["Investment", parseInt(init_investment_balance)], // change later
+    ["Investment", parseInt(investment)],
   ]);
   var options = {
     title: "",
--- routers.js
+++ routers.js
@@ -4,10 +4,9 @@ let router = express.Router();
 
 require("dotenv").config();
 const { connection } = require("./database");
-
 const { auth, requiresAuth } = require("express-openid-connect");
-const query = require("express/lib/middleware/query");
 
+router.use("/public", express.static(path.join(__dirname, "public")));
 router.use(
   auth({
     authRequired: false,
@@ -20,9 +19,6 @@ router.use(
   })
 );
 
-router.use("/public", express.static(path.join(__dirname, "public")));
-
-// ------------------- Default -------------------
 router.get("/", (request, response) => {
   request.oidc.isAuthenticated() ? response.redirect("/index") : response.redirect("/login");
 });
@@ -40,51 +36,10 @@ router.get('/profile', requiresAuth(), (request, response) => {
   response.send(fname+" "+lname+" "+email);
 });
 
-// GET balance and uid
-/*router.get("/balance", requiresAuth(), (request, response) => {
+router.get("/balance", requiresAuth(), (request, response) => {
   let userinfo = JSON.stringify(request.oidc.user);
   let email = userinfo.split('"email":"')[1].split('","')[0];
-  connection.query(`select u.id, a.balance from accounts a left join users u 
-    on a.userid = u.id where u.email='${email}'`, (error, result) => {
-      if (error) {
-        console.log(error);
-        response.status(500).send("Something went wrong...");
-      } else {
-        result.length == 0
-          ? response.status(404).send("user not found")
-          : response.status(200).send(result);
-      }
-    })
-});*/
-
-// GET user based on fname/lname/email
-router.get("/users/uid", requiresAuth(), (request, response) => {
-  let userinfo = JSON.stringify(request.oidc.user);
-  let name = userinfo.split('"name":"')[1].split('","')[0];
-  let fname = name.split(" ")[0];
-  let lname = name.split(" ")[1];
-  connection.query(
-    `select id from users where fname='${fname}' and lname='${lname}'`,
-    (error, result) => {
-      if (error) {
-        console.log(error);
-        response.status(500).send("Something went wrong...");
-      } else {
-        result.length == 0
-          ? response.status(404).send("user not found")
-          : response.status(200).send(result);
-      }
-    }
-  );
-});
-
-// ------------------- TRANSACTIONS -------------------
-// GET transaction history
-router.get("/transactions/history", requiresAuth(), (request, response) => {
-  let userinfo = JSON.stringify(request.oidc.user);
-  let email = userinfo.split('"email":"')[1].split('","')[0];
-  connection.query(`select u.id, a.balance, t.transaction_date, t.amount from accounts a
-      left join users u on a.userid = u.id left join transactions t on t.account_id = a.account_id
+  connection.query(`select u.id, a.balance from accounts a left join users u on a.userid = u.id
       where u.email='${email}'`, (error, result) => {
     if (error) {
       console.log(error);
@@ -97,7 +52,45 @@ router.get("/transactions/history", requiresAuth(), (request, response) => {
   });
 });
 
-// POST API for deposits
+router.get("/investment", requiresAuth(), (request, response) => {
+  let userinfo = JSON.stringify(request.oidc.user);
+  let email = userinfo.split('"email":"')[1].split('","')[0];
+  connection.query(`select u.id, a.balance, sum(t.amount) as invest from accounts a left join users u
+      on a.userid = u.id left join transactions t on t.account_id = a.account_id
+      where u.email='${email}' and t.type=020 group by t.type`, (error, result) => {
+    if (error) {
+      console.log(error);
+      response.status(500).send("Something went wrong...");
+    } else {
+      result.length == 0
+        ? response.status(404).send("user not found")
+        : response.status(200).send(result);
+    }
+  });
+});
+
+// GET transaction history
+router.get("/transactions/history", requiresAuth(), (request, response) => {
+  let userinfo = JSON.stringify(request.oidc.user);
+  let email = userinfo.split('"email":"')[1].split('","')[0];
+  connection.query(`select u.id, a.balance, t.transaction_date, t.amount, t.type from accounts a
+      left join users u on a.userid = u.id left join transactions t on t.account_id = a.account_id
+      where u.email='${email}';
+      select sum(t.amount) as invest from accounts a left join users u on a.userid = u.id 
+      left join transactions t on t.account_id = a.account_id where u.email='${email}'
+      and t.type in (010, 020) group by t.type order by t.type`, (error, result) => {
+    if (error) {
+      console.log(error);
+      response.status(500).send("Something went wrong...");
+    } else {
+      result.length == 0
+        ? response.status(404).send("user not found")
+        : response.status(200).send(result);
+    }
+  });
+});
+
+// top up to account balance
 router.get("/transactions/deposit", requiresAuth(), (request, response) => {
   let userinfo = JSON.stringify(request.oidc.user);
   let email = userinfo.split('"email":"')[1].split('","')[0];
@@ -107,154 +100,60 @@ router.get("/transactions/deposit", requiresAuth(), (request, response) => {
   datetime += currentdate.getMonth() + 1 + "-";
   datetime += currentdate.getDate();
 
+  let amount = request.query.amount;
   connection.query(`INSERT INTO transactions (type, amount, transaction_date, account_id)
-      VALUES ('080', ${request.query.amount}, '${datetime}', 
+      VALUES ('080', ${amount}, '${datetime}', 
       (select a.account_id from users u left join accounts a on a.userid=u.id where u.email='${email}'));
-      update accounts a left join users u on a.userid=u.id set a.balance = a.balance + ${request.query.amount}
-      where u.email='${email}'`,
-      (error, result) => {
-    if (error) {
-      console.log(error);
-      response.status(500).send("Something went wrong...");
-    } else {
-      result.length == 0
-        ? response.status(404).send("Please input deposit amount.")
-        : response.status(200).send(result);
-    }
+      update accounts a left join users u on a.userid=u.id set a.balance = a.balance + ${amount}
+      where u.email='${email}'`, (error, result) => {
+    if (amount == null) { response.status(404).send("Missing input"); }
+    else if (error) { response.status(500).send(error); }
+    else { response.status(200).send("Top up completed"); }
   });
 });
 
-// POST API for buy
+// buy investments
 router.get("/transactions/buy", requiresAuth(), (request, response) => {
+  let userinfo = JSON.stringify(request.oidc.user);
+  let email = userinfo.split('"email":"')[1].split('","')[0];
   var currentdate = new Date();
-
   var datetime = "";
   datetime += currentdate.getFullYear() + "-";
   datetime += currentdate.getMonth() + 1 + "-";
   datetime += currentdate.getDate();
 
-  let buyAmt = request.query.amount;
-
-  connection.query(
-    `INSERT INTO transactions(type, amount, transaction_date, account_id) VALUES ('010', -${buyAmt}, '${datetime}', 922)`,
-    (error, result) => {
-      if (error) {
-        console.log(error);
-        response.status(500).send("Something went wrong...");
-      } else {
-        result.length == 0
-          ? response.status(404).send("Please input amount to buy.")
-          : response.status(200).send(result);
-      }
-    }
-  );
+  let amount = request.query.amount;
+  connection.query(`INSERT INTO transactions (type, amount, transaction_date, account_id)
+      VALUES ('010', ${amount}, '${datetime}',
+      (select a.account_id from users u left join accounts a on a.userid=u.id where u.email='${email}'));
+      update accounts a left join users u on a.userid=u.id set a.balance = a.balance - ${amount}
+      where u.email='${email}'`, (error, result) => {
+    if (amount == null) { response.status(404).send("Missing input"); }
+    else if (error) { response.status(500).send(error); }
+    else { response.status(200).send("Purchase completed"); }
+  });
 });
 
-//  API for sell
+// sell investments
 router.get("/transactions/sell", requiresAuth(), (request, response) => {
+  let userinfo = JSON.stringify(request.oidc.user);
+  let email = userinfo.split('"email":"')[1].split('","')[0];
   var currentdate = new Date();
-
   var datetime = "";
   datetime += currentdate.getFullYear() + "-";
   datetime += currentdate.getMonth() + 1 + "-";
   datetime += currentdate.getDate();
 
-  let sellAmt = request.query.amount;
-
-  connection.query(
-    `INSERT INTO transactions(type, amount, transaction_date, account_id) VALUES ('010', ${sellAmt}, '${datetime}', 922)`,
-    (error, result) => {
-      if (error) {
-        console.log(error);
-        response.status(500).send("Something went wrong...");
-      } else {
-        result.length == 0
-          ? response.status(404).send("Please input amount to sell.")
-          : response.status(200).send(result);
-      }
-    }
-  );
+  let amount = request.query.amount;
+  connection.query(`INSERT INTO transactions (type, amount, transaction_date, account_id)
+      VALUES ('020', ${amount}, '${datetime}',
+      (select a.account_id from users u left join accounts a on a.userid=u.id where u.email='${email}'));
+      update accounts a left join users u on a.userid=u.id set a.balance = a.balance + ${amount}
+      where u.email='${email}'`, (error, result) => {
+    if (amount == null) { response.status(404).send("Missing input"); }
+    else if (error) { response.status(500).send(error); }
+    else { response.status(200).send("Purchase completed"); }
+  });
 });
-
-// API for getting balance from transactions table using SUM query
-router.get("/transactions/balance", requiresAuth(), (request, response) => {
-  connection.query(
-    `SELECT sum(amount) AS balance FROM transactions WHERE account_id=922`,
-    (error, result) => {
-      if (error) {
-        console.log(error);
-        response.status(500).send("Something went wrong...");
-      } else {
-        response.status(200).send(result);
-      }
-    }
-  );
-});
-
-// ------------------- USERS -------------------
-// GET user based on uid passed in the request
-router.get("/users/by-uid", requiresAuth(), (request, response) => {
-  connection.query(
-    `select * from users where uid=${request.query.uid}`,
-    (error, result) => {
-      if (error) {
-        console.log(error);
-        response.status(500).send("Something went wrong...");
-      } else {
-        result.length == 0
-          ? response.status(404).send("user not found")
-          : response.status(200).send(result);
-      }
-    }
-  );
-});
-
-// Define a POST API to add a new user to database
-/*router.post("/users/add", requiresAuth(), (request, response) => {
-  connection.query(
-    `insert into users (first_name, last_name, email) 
-    values ("${request.body.first_name}", "${request.body.last_name}", "${request.body.email}")`,
-    (error, result) => {
-      if (error) {
-        console.log(error);
-        response.status(500).send("Something went wrong...");
-      } else {
-        response.status(200).send("User added to the database!");
-      }
-    }
-  );
-});
-
-// ------------------- ACCOUNTS -------------------
-
-// Define an API to return all accounts
-router.get("/accounts/all", requiresAuth(), (request, response) => {
-  let accounts = database.get_all_accounts();
-  response.send(accounts);
-});
-
-// Define an API to return accounts when input account holder
-router.get("/accounts/by-accholder", requiresAuth(), (request, response) => {
-  let accounts = database.get_accs_by_holder(request.query.accholder);
-  response.send(accounts);
-});
-
-router.get("/accounts/by-acc-no", requiresAuth(), (request, response) => {
-  let accounts = database.get_accs_by_holder(request.query.acc_no);
-  response.send(accounts);
-}); // still not working
-
-Define a POST API to add a new account to database
-router.post("/accounts/add", requiresAuth(), (request, response) => {
-  let accounts = database.add_acc(request.body.acc_no);
-  response.send("account added.");
-});
-
-Define a DEL API
-
-router.delete("/accounts/delete", requiresAuth(), (request, response) => {
-  let account_to_del = database.del_acc_by_accNo(request.query.acc_no);
-  response.send(`Account ${account_to_del} deleted`);
-}); */
 
 module.exports = { router };
